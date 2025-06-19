2737e795aa1786ce47e42d03c1b3becbfd80e8ca
zrl <fd@facecast.live>
2025-06-19 11:21:23 +0800

    update

--- js/json.js
+++ js/json.js
@@ -2,8 +2,8 @@ core.ready(() => {
     const json = document.querySelector('.json');
     const sourceInput = json.querySelector('textarea[name=source]');
     const targetInput = json.querySelector('textarea[name=target]');
-    const compressButton = json.querySelector('input[type=button][name=compress]');
-    const formatButton = json.querySelector('input[type=button][name=format]');
+    const compressButton = json.querySelector('button[name=compress]');
+    const formatButton = json.querySelector('button[name=format]');
     compressButton.addEventListener('click', async (e) => {
         const source = sourceInput.value.trim();
         if (source == '') {
