8e84669fbf554f2f20ed69de710e14026799fb89
zrl <fd@facecast.live>
2025-06-26 21:16:39 +0800

    update

--- js/page/time.js
+++ js/page/time.js
@@ -1,9 +1,11 @@
 core.ready(() => {
     const time = document.querySelector('.time');
     const currentTimestampObject = time.querySelector('span[name=currentTimestamp]');
+    const formatTimestampObject = time.querySelector('span[name=formatTimestamp]');
     let timestampTask = setInterval(()=>onTimestampChange(), 1000);
     let onTimestampChange = () => {
         const timestampValue = core.date.current();
         currentTimestampObject.innerHTML = timestampValue;
+        formatTimestampObject.innerHTML = moment().millisecond(timestampValue).format('YYYY-MM-DD HH:mm:ss');
     };
 });
